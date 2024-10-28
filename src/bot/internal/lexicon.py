d68c7df2b390b88ff20bab01beae8dae8cdc705d
vas g <vas.g@me.com>
2024-10-28 16:00:06 +0400

    working on internal stuff

--- src/bot/internal/lexicon.py
+++ src/bot/internal/lexicon.py
@@ -2,6 +2,13 @@
 texts = {
     'start_message': 'Hello, {user_fullname}!\n\nThis is a bot for connecting to VPN',
     'invoice_message': 'Get demo access for 7 days',
-
+    'user_created': 'Welcome, {user_fullname}\n\nYour ID: <code>{user_id}</code>\n'
+                    '├ your status: <b>{status}</b>\n'
+                    '├ proxy type: <b>{proxy_type}</b>\n'
+                    '├ valid until: <b>{valid_until}</b>\n'
+                    '└ days left: <b>{days_left}</b>\n\n'
+                    '<b>Links:</b>',
+    'links_message': '<pre>{link1}\n\n'
+                     '{link2}</pre>',
 }
 # fmt: on
