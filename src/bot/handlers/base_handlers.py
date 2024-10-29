26c5a183157f74d76702c9bc0f1a8857d69a47e0
vas g <vas.g@me.com>
2024-10-29 21:19:42 +0400

    working on handlers

--- src/bot/handlers/base_handlers.py
+++ src/bot/handlers/base_handlers.py
@@ -1,14 +1,21 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import (
     CommandStart,
 )
-from aiogram.types import LabeledPrice, Message
+from aiogram.types import CallbackQuery, LabeledPrice, Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from bot.controllers.crud.link import get_links_by_user_id
 from bot.internal.enums import SubscriptionPrice
-from bot.internal.keyboards import demo_access_kb
+from bot.internal.keyboards import (
+    SubscriptionCallbackFactory,
+    buy_subscription_kb,
+    close_links_kb,
+    connect_vpn_kb,
+    show_links_kb,
+)
 from bot.internal.lexicon import texts
 from bot.controllers.crud.user import get_user_from_db_by_tg_id
 
@@ -18,16 +25,13 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message, db_session: AsyncSession) -> None:
     user = await get_user_from_db_by_tg_id(message.from_user.id, db_session)
-    current_time = datetime.now()
+    current_time = datetime.now(UTC)
     if not user:
-        await message.answer(texts['start_message'].format(
-            user_fullname=message.from_user.full_name,
-        ))
         prices = [LabeledPrice(label="XTR", amount=SubscriptionPrice.ONE_WEEK_DEMO_ACCESS.value)]
         await message.answer_invoice(
             title="Demo access to VPN",
             description="For access to VPN, please purchase a subscription",
-            payload="payment for VPN",
+            payload="demo access",
             currency="XTR",
             prices=prices,
             start_parameter="stars-payment",
@@ -35,28 +39,75 @@ async def start(message: Message, db_session: AsyncSession) -> None:
     else:
         if user.expired_at > message.date:
             delta = user.expired_at - current_time
-            await message.answer(texts['start_message'].format(
-                user_fullname=message.from_user.full_name,
-                user_id=message.from_user.id,
-                status='Inactive',
-                proxy_type='VMess',
-                valid_until=user.expired_at,
-                days_left=delta.days
-            ))
-        else:
-            prices = [
-                LabeledPrice(label="XTR", amount=SubscriptionPrice.ONE_MONTH_SUBSCRIPTION.value),
-                LabeledPrice(label="XTR", amount=SubscriptionPrice.SIX_MONTH_SUBSCRIPTION.value),
-                LabeledPrice(label="XTR", amount=SubscriptionPrice.ONE_YEAR_SUBSCRIPTION.value)
-            ]
-            await message.answer_invoice(
-                title="Add more time",
-                description="For access to VPN, please purchase a subscription",
-                payload="payment for VPN",
-                currency="XTR",
-                prices=prices,
-                start_parameter="stars-payment",
+            formatted_date = user.expired_at.strftime("%d.%m.%Y")
+            await message.answer(
+                texts['start_message'].format(
+                    user_fullname=message.from_user.full_name,
+                    user_id=message.from_user.id,
+                    status='Active',
+                    proxy_type='VMess',
+                    valid_until=formatted_date,
+                    days_left=delta.days,
+                ),
+                reply_markup=show_links_kb(),
             )
-            # await message.answer(
-            #     texts['start_message'].format(message.from_user.full_name), reply_markup=demo_access_kb()
-            # )
+        else:
+            await message.answer(
+                texts['user_expired'].format(
+                    user_fullname=message.from_user.full_name, status='Inactive', reply_markup=connect_vpn_kb()
+                )
+            )
+
+
+@router.callback_query(SubscriptionCallbackFactory.filter())
+async def subscription_prolongation(
+    callback: CallbackQuery,
+    callback_data: SubscriptionCallbackFactory,
+) -> None:
+    await callback.answer()
+    match callback_data.stars_amount:
+        case SubscriptionPrice.ONE_MONTH_SUBSCRIPTION:
+            prices = [LabeledPrice(label="XTR", amount=SubscriptionPrice.ONE_MONTH_SUBSCRIPTION.value)]
+            description = 'Access to VPN for 1 month'
+        case SubscriptionPrice.SIX_MONTH_SUBSCRIPTION:
+            prices = [LabeledPrice(label="XTR", amount=SubscriptionPrice.SIX_MONTH_SUBSCRIPTION.value)]
+            description = 'Access to VPN for 6 month'
+        case SubscriptionPrice.ONE_YEAR_SUBSCRIPTION:
+            prices = [LabeledPrice(label="XTR", amount=SubscriptionPrice.ONE_YEAR_SUBSCRIPTION.value)]
+            description = 'Access to VPN for 12 month'
+        case _:
+            assert False, f'unexpected value {callback_data.stars_amount}'
+    await callback.message.answer_invoice(
+        title="Access to VPN",
+        description=description,
+        payload="payment for VPN",
+        currency="XTR",
+        prices=prices,
+        start_parameter="stars-payment",
+    )
+
+
+@router.callback_query(F.data == 'show links')
+async def show_links(callback: CallbackQuery, db_session: AsyncSession) -> None:
+    await callback.answer()
+    links = await get_links_by_user_id(callback.from_user.id, db_session)
+    await callback.message.answer(
+        texts['links_message'].format(links='\n\n'.join(i for i in links)), reply_markup=close_links_kb()
+    )
+
+
+@router.callback_query(F.data == 'close links')
+async def close_links(callback: CallbackQuery) -> None:
+    await callback.message.delete()
+
+
+@router.callback_query(F.data == 'connect')
+async def close_links(callback: CallbackQuery) -> None:
+    await callback.answer()
+    await callback.message.answer(
+        texts['user_expired'].format(
+            user_fullname=callback.from_user.full_name,
+            status='Inactive',
+        ),
+        reply_markup=buy_subscription_kb(),
+    )
--- src/bot/handlers/payment_handlers.py
+++ src/bot/handlers/payment_handlers.py
@@ -7,8 +7,9 @@ from aiogram.types import Message, PreCheckoutQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from bot.internal.enums import SubscriptionPrice
+from bot.internal.keyboards import show_links_kb
 from bot.internal.lexicon import texts
-from bot.controllers.marzban import create_marzban_user, get_marzban_token
+from bot.controllers.marzban import create_marzban_user, get_marzban_token, process_subscription
 from bot.controllers.crud.user import get_user_from_db_by_tg_id, update_user_expiration
 from database.models import Link, User
 
@@ -30,69 +31,61 @@ async def on_successful_payment(
     user = await get_user_from_db_by_tg_id(message.from_user.id, db_session)
     payment_info = message.successful_payment
     total_amount = payment_info.total_amount
+    current_timestamp = int(datetime.now().timestamp())
     username = (
         '@' + message.from_user.username
         if message.from_user.username
         else message.from_user.full_name.replace(' ', '_')
     )
-    match total_amount:
-        case SubscriptionPrice.ONE_WEEK_DEMO_ACCESS:
-            if not user:
-                marzban_secret_token = await get_marzban_token()
-                new_marzban_user = await create_marzban_user(username, message.from_user.id, marzban_secret_token)
-                expire_timestamp = new_marzban_user.get("expire")
-                expire_date = datetime.fromtimestamp(expire_timestamp)
-                current_timestamp = int(datetime.now().timestamp())
-                days_left = (expire_timestamp - current_timestamp) // (24 * 3600)
-                tcp_link = new_marzban_user.get("links")[0]
-                websocket_link = new_marzban_user.get("links")[1]
-                await message.answer(
-                    texts['user_created'].format(
-                        user_fullname=message.from_user.full_name,
-                        user_id=message.from_user.id,
-                        status=new_marzban_user.get("status"),
-                        proxy_type='VMess',
-                        valid_until=expire_date.strftime("%d.%m.%Y"),
-                        days_left=days_left,
-                    )
-                )
-                await message.answer(
-                    texts['links_message'].format(
-                        link1=tcp_link,
-                        link2=websocket_link,
-                    )
-                )
-                new_user = User(
-                    tg_id=message.from_user.id,
-                    fullname=message.from_user.full_name,
-                    username=message.from_user.username,
-                    demo_access_used=True,
-                    expired_at=expire_date,
-                )
-                new_tcp_link = Link(
-                    user_marzban_id=new_marzban_user["proxies"]["vmess"]["id"],
-                    user_tg_id=message.from_user.id,
-                    url=tcp_link,
-                )
-                new_websocket_link = Link(
-                    user_marzban_id=new_marzban_user["proxies"]["vmess"]["id"],
-                    user_tg_id=message.from_user.id,
-                    url=websocket_link,
-                )
-                db_session.add(new_user)
-                await db_session.flush()
-                db_session.add_all([new_tcp_link, new_websocket_link])
-        case SubscriptionPrice.ONE_MONTH_SUBSCRIPTION:
-            await update_user_expiration(user, 1, db_session)
-        case SubscriptionPrice.SIX_MONTH_SUBSCRIPTION:
-            await update_user_expiration(user, 6, db_session)
-        case SubscriptionPrice.ONE_YEAR_SUBSCRIPTION:
-            await update_user_expiration(user, 12, db_session)
-        case _:
-            assert False, f"Unexpected total_amount: {total_amount}"
+    if total_amount == SubscriptionPrice.ONE_WEEK_DEMO_ACCESS:
+        if not user:
+            marzban_secret_token = await get_marzban_token()
+            new_marzban_user = await create_marzban_user(username, message.from_user.id, marzban_secret_token)
+            expire_timestamp = new_marzban_user.get("expire")
+            expire_date = datetime.fromtimestamp(expire_timestamp)
+            days_left = (expire_timestamp - current_timestamp) // (24 * 3600)
+            tcp_link, websocket_link, *_ = new_marzban_user.get("links")
+
+            new_user = User(
+                tg_id=message.from_user.id,
+                fullname=message.from_user.full_name,
+                username=message.from_user.username,
+                marzban_username=new_marzban_user.get("username"),
+                demo_access_used=True,
+                expired_at=expire_date,
+            )
+            db_session.add(new_user)
+            await db_session.flush()
+
+            db_session.add_all(
+                [
+                    Link(user_tg_id=message.from_user.id, url=tcp_link),
+                    Link(user_tg_id=message.from_user.id, url=websocket_link),
+                ]
+            )
+
+            text = texts['user_created'].format(
+                user_fullname=message.from_user.full_name,
+                user_id=message.from_user.id,
+                status=new_marzban_user.get("status"),
+                proxy_type='VMess',
+                valid_until=expire_date.strftime("%d.%m.%Y"),
+                days_left=days_left,
+            )
+    else:
+        match total_amount:
+            case SubscriptionPrice.ONE_MONTH_SUBSCRIPTION:
+                text = await process_subscription(user, 1, current_timestamp, db_session)
+            case SubscriptionPrice.SIX_MONTH_SUBSCRIPTION:
+                text = await process_subscription(user, 6, current_timestamp, db_session)
+            case SubscriptionPrice.ONE_YEAR_SUBSCRIPTION:
+                text = await process_subscription(user, 12, current_timestamp, db_session)
+            case _:
+                raise ValueError(f"Unexpected total_amount: {total_amount}")
+
     await message.answer(
-        "Payment successful!",
-        message_effect_id="5104841245755180586",
+        text=text,
+        reply_markup=show_links_kb(),
     )
 
 
