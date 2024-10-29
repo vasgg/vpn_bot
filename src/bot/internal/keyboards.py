48d70623aa1c6f94dc6f9f14faf7f5f4143c04ee
vas g <vas.g@me.com>
2024-10-29 21:19:51 +0400

    working on internal stuff

--- src/bot/internal/keyboards.py
+++ src/bot/internal/keyboards.py
@@ -1,5 +1,12 @@
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from bot.internal.enums import SubscriptionPrice
+
+
+class SubscriptionCallbackFactory(CallbackData, prefix='subscription'):
+    stars_amount: SubscriptionPrice
 
 
 def demo_access_kb() -> InlineKeyboardMarkup:
@@ -10,3 +17,51 @@ def demo_access_kb() -> InlineKeyboardMarkup:
         callback_data="1z",
     )
     return builder.as_markup()
+
+
+def buy_subscription_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    for text, callback in [
+        ("1 month", SubscriptionCallbackFactory(stars_amount=SubscriptionPrice.ONE_MONTH_SUBSCRIPTION)),
+        ("6 months", SubscriptionCallbackFactory(stars_amount=SubscriptionPrice.SIX_MONTH_SUBSCRIPTION)),
+        ("12 months", SubscriptionCallbackFactory(stars_amount=SubscriptionPrice.ONE_YEAR_SUBSCRIPTION)),
+    ]:
+        kb.button(text=text, callback_data=callback)
+    kb.adjust(1)
+    return kb.as_markup()
+
+
+def show_links_kb() -> InlineKeyboardMarkup:
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(
+                text="Show links",
+                callback_data="show links",
+            )
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+
+def close_links_kb() -> InlineKeyboardMarkup:
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(
+                text="Close",
+                callback_data="close links",
+            )
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+
+def connect_vpn_kb() -> InlineKeyboardMarkup:
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(
+                text="Connect",
+                callback_data="connect",
+            )
+        ]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
--- src/bot/internal/lexicon.py
+++ src/bot/internal/lexicon.py
@@ -1,19 +1,28 @@
 # fmt: off
 texts = {
-    'start_message': 'Hello, {user_fullname}!\n\nThis bot helps you connect to VPN',
+    'start_message': 'Hello, {user_fullname}!\n'
+                     '├ your status: <b>{status}</b>\n'
+                     '├ proxy type: <b>{proxy_type}</b>\n'
+                     '├ valid until: <b>{valid_until}</b>\n'
+                     '└ days left: <b>{days_left}</b>\n\n',
     'user_created': 'Welcome, {user_fullname}, you have 1 week demo access!\n\nYour ID: <code>{user_id}</code>\n'
                     '├ your status: <b>{status}</b>\n'
                     '├ proxy type: <b>{proxy_type}</b>\n'
                     '├ valid until: <b>{valid_until}</b>\n'
-                    '└ days left: <b>{days_left}</b>\n\n'
-                    '<b>Links:</b>',
-    'links_message': '<pre>{link1}\n\n'
-                     '{link2}</pre>',
+                    '└ days left: <b>{days_left}</b>\n\n',
+    'links_message': '<pre>{links}</pre>',
     'user_not_expired': 'Hello, {user_fullname}!\n\n{Your ID: <code>{user_id}</code>\n'
                         '├ your status: <b>{status}</b>\n'
                         '├ proxy type: <b>{proxy_type}</b>\n'
                         '├ valid until: <b>{valid_until}</b>\n'
                         '└ days left: <b>{days_left}</b>\n\n',
+    'user_expired': 'Hello, {user_fullname}!\n'
+                    '└ your status: <b>{status}</b>\n',
+    'renew_subscription': 'Successfully added {month_amount} months to your subscription.\n'
+                          '├ your status: <b>{status}</b>\n'
+                          '├ proxy type: <b>{proxy_type}</b>\n'
+                          '├ valid until: <b>{valid_until}</b>\n'
+                          '└ days left: <b>{days_left}</b>\n\n',
 
 }
 # fmt: on
