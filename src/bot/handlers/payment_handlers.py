a76acbb35e47006b2110e44e4b4a8a32f06a09db
vas g <vas.g@me.com>
2024-11-01 01:43:33 +0400

    working on handlers

--- src/bot/handlers/payment_handlers.py
+++ src/bot/handlers/payment_handlers.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import UTC, datetime
 
 from aiogram import Bot, F, Router
 from aiogram.exceptions import TelegramBadRequest
@@ -6,11 +6,15 @@ from aiogram.filters import Command, CommandObject
 from aiogram.types import Message, PreCheckoutQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from bot.internal.enums import SubscriptionPrice
-from bot.internal.keyboards import show_links_kb
-from bot.internal.lexicon import texts
-from bot.controllers.marzban import create_marzban_user, get_marzban_token, process_subscription
-from bot.controllers.crud.user import get_user_from_db_by_tg_id, update_user_expiration
+from bot.controllers.crud.user import update_db_user_expiration
+from bot.internal.enums import SubscriptionPlan
+from bot.internal.keyboards import connect_vpn_kb
+from bot.internal.dicts import goods, texts
+from bot.controllers.marzban import (
+    create_marzban_user,
+    get_duration_string, get_marzban_token,
+    update_marzban_user_expiration,
+)
 from database.models import Link, User
 
 router = Router()
@@ -26,66 +30,64 @@ async def on_pre_checkout_query(
 @router.message(F.successful_payment)
 async def on_successful_payment(
     message: Message,
+    user: User,
     db_session: AsyncSession,
 ):
-    user = await get_user_from_db_by_tg_id(message.from_user.id, db_session)
-    payment_info = message.successful_payment
-    total_amount = payment_info.total_amount
-    current_timestamp = int(datetime.now().timestamp())
     username = (
         '@' + message.from_user.username
         if message.from_user.username
         else message.from_user.full_name.replace(' ', '_')
     )
-    if total_amount == SubscriptionPrice.ONE_WEEK_DEMO_ACCESS:
-        if not user:
-            marzban_secret_token = await get_marzban_token()
-            new_marzban_user = await create_marzban_user(username, message.from_user.id, marzban_secret_token)
-            expire_timestamp = new_marzban_user.get("expire")
-            expire_date = datetime.fromtimestamp(expire_timestamp)
-            days_left = (expire_timestamp - current_timestamp) // (24 * 3600)
-            tcp_link, websocket_link, *_ = new_marzban_user.get("links")
-
-            new_user = User(
-                tg_id=message.from_user.id,
-                fullname=message.from_user.full_name,
-                username=message.from_user.username,
-                marzban_username=new_marzban_user.get("username"),
-                demo_access_used=True,
-                expired_at=expire_date,
-            )
-            db_session.add(new_user)
-            await db_session.flush()
-
-            db_session.add_all(
-                [
-                    Link(user_tg_id=message.from_user.id, url=tcp_link),
-                    Link(user_tg_id=message.from_user.id, url=websocket_link),
-                ]
-            )
-
-            text = texts['user_created'].format(
-                user_fullname=message.from_user.full_name,
-                user_id=message.from_user.id,
-                status=new_marzban_user.get("status"),
-                proxy_type='VMess',
-                valid_until=expire_date.strftime("%d.%m.%Y"),
-                days_left=days_left,
-            )
+    current_timestamp = int(datetime.now(UTC).timestamp())
+    payload = message.successful_payment.invoice_payload
+    if payload == SubscriptionPlan.ONE_WEEK_DEMO_ACCESS:
+        user.demo_access_used = True
+    duration = goods[payload]['duration']
+    marzban_token = await get_marzban_token()
+    if not user.marzban_username:
+        new_marzban_user = await create_marzban_user(username, message.from_user.id, marzban_token, duration)
+        expire_timestamp = new_marzban_user.get("expire")
+        expire_date = datetime.fromtimestamp(expire_timestamp)
+        days_left = (expire_timestamp - current_timestamp) // (24 * 3600)
+        tcp_link, websocket_link, *_ = new_marzban_user.get("links")
+        user.marzban_username = new_marzban_user.get("username")
+        user.expired_at = expire_date
+        db_session.add_all(
+            [
+                Link(user_tg_id=message.from_user.id, url=tcp_link),
+                Link(user_tg_id=message.from_user.id, url=websocket_link),
+            ]
+        )
+        text = texts['user_created'].format(
+            user_fullname=message.from_user.full_name,
+            user_id=message.from_user.id,
+            proxy_type='VMess',
+            valid_until=expire_date.strftime("%d.%m.%Y"),
+            days_left=days_left,
+        )
     else:
-        match total_amount:
-            case SubscriptionPrice.ONE_MONTH_SUBSCRIPTION:
-                text = await process_subscription(user, 1, current_timestamp, db_session)
-            case SubscriptionPrice.SIX_MONTH_SUBSCRIPTION:
-                text = await process_subscription(user, 6, current_timestamp, db_session)
-            case SubscriptionPrice.ONE_YEAR_SUBSCRIPTION:
-                text = await process_subscription(user, 12, current_timestamp, db_session)
-            case _:
-                raise ValueError(f"Unexpected total_amount: {total_amount}")
+        await update_marzban_user_expiration(user.marzban_username, marzban_token, duration, user.expired_at)
+        await update_db_user_expiration(user, duration, db_session)
 
+        if user.expired_at > message.date:
+            text = texts['prolong_subscription'].format(
+                added_time=get_duration_string(duration),
+                user_id=message.from_user.id,
+                proxy_type='VMess',
+                valid_until=user.expired_at.strftime("%d.%m.%Y"),
+                days_left=(user.expired_at - message.date).days,
+            )
+        else:
+            text = texts['renew_subscription'].format(
+                added_time=get_duration_string(duration),
+                user_id=message.from_user.id,
+                proxy_type='VMess',
+                valid_until=user.expired_at.strftime("%d.%m.%Y"),
+                days_left=(user.expired_at - message.date).days,
+            )
     await message.answer(
         text=text,
-        reply_markup=show_links_kb(),
+        reply_markup=connect_vpn_kb(with_links=True),
     )
 
 
