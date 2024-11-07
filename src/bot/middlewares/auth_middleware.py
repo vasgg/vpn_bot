8c687a57011d912fe5db8c02b1dd2908af3aaa30
vas g <vas.g@me.com>
2024-11-07 23:54:38 +0400

    working on middleware

--- src/bot/middlewares/auth_middleware.py
+++ src/bot/middlewares/auth_middleware.py
@@ -5,6 +5,7 @@ from aiogram import BaseMiddleware
 from aiogram.types import Message
 
 from bot.controllers.crud.user import add_user_to_db, get_user_from_db_by_tg_id
+from bot.internal.dicts import texts
 
 
 class AuthMiddleware(BaseMiddleware):
@@ -18,6 +19,7 @@ class AuthMiddleware(BaseMiddleware):
         user = await get_user_from_db_by_tg_id(event.from_user.id, db_session)
         if not user:
             user = await add_user_to_db(event.from_user, db_session)
+            await event.answer(texts['welcome'].format(username=user.fullname))
         user_active = user.expired_at and user.expired_at.replace(tzinfo=UTC) > datetime.now(UTC)
         data['user'] = user
         data['user_active'] = user_active
