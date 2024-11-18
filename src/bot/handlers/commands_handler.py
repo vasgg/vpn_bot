2cdfa36e947b811e530c9abd82234ab16ecfde34
niko-on-telegram <nikoontelegram@gmail.com>
2024-11-18 11:48:32 +0300

    renew fix, added internal host to bot

--- src/bot/handlers/commands_handler.py
+++ src/bot/handlers/commands_handler.py
@@ -33,39 +33,48 @@ async def command_handler(
     user_active: bool,
     db_session: AsyncSession,
 ) -> None:
-    match command.command:
-        case "start":
-            markup = connect_vpn_kb()
-            if not user.marzban_username:
-                text = compose_message(user, message, SubscriptionStatus.INACTIVE)
-            else:
-                if user.expired_at.replace(tzinfo=UTC) > message.date.replace(tzinfo=UTC):
-                    text = compose_message(user, message, SubscriptionStatus.ACTIVE)
-                    markup = connect_vpn_kb(active=True)
-                else:
+    async def produce_command_response():
+        match command.command:
+            case "start":
+                markup = connect_vpn_kb()
+                if not user.marzban_username:
                     text = compose_message(user, message, SubscriptionStatus.INACTIVE)
-            logger.info(f"Start command called by user {user.username}")
-        case "connect_vpn":
-            text = texts['choose_plan']
-            markup = buy_subscription_kb(user.demo_access_used)
-            logger.info(f"Connect VPN command called by user {user.username}")
-        case "renew_links":
-            await renew_links(message, user, settings, db_session)
-            text = texts['links_refreshed']
-            markup = show_links_kb()
-            logger.info(f"Renew links command called by user {user.username}")
-        case "account":
-            user_status = SubscriptionStatus.ACTIVE if user_active else SubscriptionStatus.INACTIVE
-            text = compose_message(user, message, user_status)
-            markup = account_kb()
-            logger.info(f"Account command called by user {user.username}")
-        case "help":
-            text = texts['help'].format(user_id=user.tg_id)
-            markup = help_menu_kb()
-            logger.info(f"Help command called by user {user.username}")
-        case _:
-            assert False, "Unexpected command"
+                else:
+                    if user.expired_at.replace(tzinfo=UTC) > message.date.replace(tzinfo=UTC):
+                        text = compose_message(user, message, SubscriptionStatus.ACTIVE)
+                        markup = connect_vpn_kb(active=True)
+                    else:
+                        text = compose_message(user, message, SubscriptionStatus.INACTIVE)
+                logger.info(f"Start command called by user {user.username}")
+            case "connect_vpn":
+                text = texts['choose_plan']
+                markup = buy_subscription_kb(user.demo_access_used)
+                logger.info(f"Connect VPN command called by user {user.username}")
+            case "renew_links":
+                if user.marzban_username is None:
+                    # forwarding to connect_vpn logic
+                    text = texts['choose_plan']
+                    markup = buy_subscription_kb(user.demo_access_used)
+                    return text, markup
+                await renew_links(message, user, settings, db_session)
+                text = texts['links_refreshed']
+                markup = show_links_kb()
+                logger.info(f"Renew links command called by user {user.username}")
+            case "account":
+                user_status = SubscriptionStatus.ACTIVE if user_active else SubscriptionStatus.INACTIVE
+                text = compose_message(user, message, user_status)
+                markup = account_kb()
+                logger.info(f"Account command called by user {user.username}")
+            case "help":
+                text = texts['help'].format(user_id=user.tg_id)
+                markup = help_menu_kb()
+                logger.info(f"Help command called by user {user.username}")
+            case _:
+                raise RuntimeError("Unexpected command")
+        return text, markup
+
+    text_, markup_ = produce_command_response()
     await message.answer(
-        text=text,
-        reply_markup=markup,
+        text=text_,
+        reply_markup=markup_,
     )
